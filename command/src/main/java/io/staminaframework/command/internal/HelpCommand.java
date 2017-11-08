99fa5dafdb64663f4a4d6f7ba8d7a02f9c0f56d5
Alexandre Roman <alexandre.roman@gmail.com>
2017-11-08 22:35:09 +0100

    Fix command help

--- command/src/main/java/io/staminaframework/command/internal/HelpCommand.java
+++ command/src/main/java/io/staminaframework/command/internal/HelpCommand.java
@@ -19,15 +19,17 @@ package io.staminaframework.command.internal;
 import io.staminaframework.command.Command;
 import io.staminaframework.command.CommandConstants;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Command printing help about an other command.
@@ -59,33 +61,52 @@ public class HelpCommand implements Command {
     public boolean execute(Context ctx) throws Exception {
         final String cmdName = ctx.arguments().length == 0 ? null : ctx.arguments()[0];
         if (cmdName == null) {
-            final Collection<ServiceReference<Command>> commandRefs =
-                    bundleContext.getServiceReferences(Command.class, null);
-            final List<String> commandNames = new ArrayList<>(commandRefs.size());
-            for (final ServiceReference<Command> ref : commandRefs) {
-                final Object rawCommandName = ref.getProperty(CommandConstants.COMMAND);
-                if (rawCommandName != null && rawCommandName instanceof String) {
-                    commandNames.add((String) rawCommandName);
+            final List<String> commandNames = new CopyOnWriteArrayList<>();
+            final ServiceTracker<Command, Command> tracker =
+                    new ServiceTracker<>(bundleContext, Command.class, new ServiceTrackerCustomizer<Command, Command>() {
+                        @Override
+                        public Command addingService(ServiceReference<Command> reference) {
+                            final String name = (String) reference.getProperty(CommandConstants.COMMAND);
+                            if (name != null) {
+                                commandNames.add(name);
+                            }
+                            return null;
+                        }
+
+                        @Override
+                        public void modifiedService(ServiceReference<Command> reference, Command service) {
+                        }
+
+                        @Override
+                        public void removedService(ServiceReference<Command> reference, Command service) {
+                        }
+                    });
+            tracker.open();
+            try {
+                synchronized (tracker) {
+                    tracker.wait(4000);
                 }
+            } finally {
+                tracker.close();
             }
+
             ctx.out().println("Available commands:");
-            commandNames.stream().sorted().forEach(ctx.out()::println);
+            commandNames.stream().sorted().distinct().forEach(ctx.out()::println);
         } else {
-            final Collection<ServiceReference<Command>> refs =
-                    bundleContext.getServiceReferences(Command.class, "(" + CommandConstants.COMMAND + "=" + cmdName + ")");
-            if (refs.isEmpty()) {
-                ctx.err().println("Command not found: " + cmdName);
-            } else {
-                if (refs.size() > 1) {
-                    ctx.err().println("More than one command found: showing first");
-                }
-                final ServiceReference<Command> ref = refs.iterator().next();
-                try {
-                    final Command cmd = bundleContext.getService(ref);
+            final String commandFilter = "(&(" + Constants.OBJECTCLASS + "=" + Command.class.getName()
+                    + ")(" + CommandConstants.COMMAND + "=" + cmdName + "))";
+            final ServiceTracker<Command, Command> tracker =
+                    new ServiceTracker<>(bundleContext, bundleContext.createFilter(commandFilter), null);
+            tracker.open();
+            try {
+                final Command cmd = tracker.waitForService(4000);
+                if (cmd == null) {
+                    ctx.err().println("Command not found: " + cmdName);
+                } else {
                     cmd.help(ctx.out());
-                } finally {
-                    bundleContext.ungetService(ref);
                 }
+            } finally {
+                tracker.close();
             }
         }
         return false;
